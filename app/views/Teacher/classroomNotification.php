971627e7ffc713b7a88da9e92bd4927c73df930f
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-02-05 22:16:29 -0500

    Teacher alert__views created

--- app/views/Teacher/classroomNotification.php
+++ app/views/Teacher/classroomNotification.php
@@ -12,6 +12,7 @@
 </head>
 
 <body>
+    
     <div class="message-area">
         <div class="message">
             <div class="message-text">
---
+++ public/components/alerts/Teacher/bmc_success.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="<?php echo BASEURL?>stylesheets/alerts_styles.css" />
+<div class="message-area">
+    <div class="message">
+        <div class="message-text">
+            You have contributed to our community, Thank you !
+        </div>
+        <div class="message-image">
+            <img src="<?php echo BASEURL?>assets/Teacher/clips/Thankyou.png" alt="error image">
+            
+        </div>
+        <div class="message-btn">
+        <a href="<?php echo BASEURL ?>privateclass/bmc">OK</a> 
+        </div>
+    </div>
+</div>
