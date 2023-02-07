525bb03207a1f9d46a9594d80532d73724c72ebe
Sanduni Gunawardhana <sandugunawardhana99@gmail.com>
2023-02-07 22:55:29 +0530

    admin alerts views

---
+++ public/components/alerts/admin/reciept_download.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="<?php echo BASEURL?>stylesheets/alerts_styles.css" />
+<div class="message-area">
+    <div class="message">
+        <div class="message-text">
+            Reciept has been downloaded Successfully !
+        </div>
+        <div class="message-image">
+            <img src="<?php echo BASEURL?>assets/admin/download.png" alt="error image">
+            
+        </div>
+        <div class="message-btn">
+        <a href="<?php echo BASEURL ?>privateclass/bmc">OK</a> 
+        </div>
+    </div>
+</div>
---
+++ public/components/alerts/admin/resource_declined.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="<?php echo BASEURL?>stylesheets/alerts_styles.css" />
+<div class="message-area">
+    <div class="message">
+        <div class="message-text">
+            Resource Declined !
+        </div>
+        <div class="message-image">
+            <img src="<?php echo BASEURL?>assets/admin/resource_decline.png" alt="error image">
+            
+        </div>
+        <div class="message-btn">
+        <a href="<?php echo BASEURL ?>privateclass/bmc">OK</a> 
+        </div>
+    </div>
+</div>
