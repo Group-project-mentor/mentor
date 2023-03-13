4a002c61100aeaf678734470886b86f2358af7a5
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-03-13 12:18:01 -0400

    some changes of teacher

--- app/views/Teacher/classRoom/createClass.php
+++ app/views/Teacher/classRoom/createClass.php
@@ -6,29 +6,71 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teacher class</title>
-    <link rel="stylesheet" href="<?php echo BASEURL?>public/stylesheets/Teacher/style.css">
-    <link rel="stylesheet" href="<?php echo BASEURL?>public/stylesheets/Teacher/card_set.css">
+    <link rel="stylesheet" href="<?php echo BASEURL ?>public/stylesheets/Teacher/style.css">
+    <link rel="stylesheet" href="<?php echo BASEURL ?>public/stylesheets/Teacher/card_set.css">
 </head>
 
 <body>
     <section class="page">
         <!-- Navigation panel -->
-        <?php include_once "components/navbars/t_nav_1.php"?>
+        <nav class="nav-bar" id="nav-bar">
+
+            <!-- Navigation bar logos -->
+            <div class="nav-upper">
+                <div class="nav-logo-short">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/logo2.png" alt="logo" />
+                </div>
+                <div class="nav-logo-long" id="nav-logo-long">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/logo1.png" alt="logo" />
+                </div>
+            </div>
+
+
+
+            <!-- Navigation buttons -->
+            <div class="nav-links">
+                <a href="<?php echo BASEURL ?>" class="nav-link">
+                    <img class="active" src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_class.png" alt="home">
+                    <div class="nav-link-text">Classes</div>
+                </a>
+                <a href="<?php echo BASEURL ?>TPremium/premiumPlan" class="nav-link">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_premium.png" alt="cource">
+                    <div class="nav-link-text">Buy Premium</div>
+                </a>
+                <a href="<?php echo BASEURL ?>TReportIssue/reportIssues" class="nav-link">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_report.png" alt="profile">
+                    <div class="nav-link-text">Report Issue</div>
+                </a>
+                <a href="<?php echo BASEURL ?>TBilling/Billing1" class="nav-link">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_billing.png" alt="report">
+                    <div class="nav-link-text">Billing</div>
+                </a>
+                <a href="<?php echo BASEURL ?>TBmc/bmc1" class="nav-link">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_bmc.png" alt="bmc">
+                    <div class="nav-link-text">Buy me a coffee</div>
+                </a>
+            </div>
+
+            <!-- Navigation bar toggler -->
+            <div class="nav-toggler" id="nav-toggler">
+                <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/toggler.png" alt="toggler">
+            </div>
+        </nav>
 
         <div class="content-area">
 
             <!-- Top bar -->
             <section class="top-bar">
-                
+
                 <div class="top-bar-btns">
-                    <a href="<?php echo BASEURL?>home">
+                    <a href="<?php echo BASEURL ?>home">
                         <div class="back-btn">Back</div>
                     </a>
                     <a href="#">
-                        <img src="<?php echo BASEURL?>public/assets/Teacher/icons/icon_notify.png" alt="notify">
+                        <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_notify.png" alt="notify">
                     </a>
-                    <a href="<?php echo  BASEURL ?>TProfile/profile">
-                        <img src="<?php echo BASEURL?>public/assets/Teacher/icons/icon_profile_black.png" alt="profile">
+                    <a href="<?php echo  BASEURL ?>privateclass/profile">
+                        <img src="<?php echo BASEURL ?>public/assets/Teacher/icons/icon_profile_black.png" alt="profile">
                     </a>
                 </div>
             </section>
@@ -36,7 +78,7 @@
             <!-- Middle part for whole content -->
             <section class="mid-content">
 
-                 <!-- Title and sub title of middle part -->
+                <!-- Title and sub title of middle part -->
                 <div class="mid-title">
                     <h1>Create Class</h1>
                     <h6>Teacher Home/ Create Class</h6>
@@ -45,28 +87,59 @@
                 </div>
 
                 <div class="class section">
-                    <form action="<?php echo BASEURL;?>privateclass/createAction" method="POST">
-                      <label for="class_name"></label>
-                      <input type="text" id="class_name" name="class_name" placeholder="New class name..">
-                      <input type="submit" value="create">
+                    <form action="<?php echo BASEURL; ?>TClassRoom/createAction" method="POST">
+                        <label for="class_name"></label>
+                        <input type="text" id="class_name" name="class_name" placeholder="New class name..">
+                        <input type="submit" value="create">
                     </form>
-                  </div>
-                  
+                </div>
 
             </section>
 
             <!-- bottom part -->
             <section class="Teacher-class-bottom">
                 <div class="Teacher-decorator">
-                    <img src="<?php echo BASEURL?>public/assets/Teacher/clips/lap_man.png" alt="lap man">
+                    <img src="<?php echo BASEURL ?>public/assets/Teacher/clips/lap_man.png" alt="lap man">
                 </div>
             </section>
 
 
-            
-        </div>  
-    </section>      
-</body>
 
+        </div>
+    </section>
+</body>
+<script>
+    let toggle = true;
+
+    const getElement = (id) => document.getElementById(id);
+
+    let togglerBtn = getElement("nav-toggler");
+    let nav = getElement("nav-bar");
+    let logoLong = getElement("nav-logo-long");
+    let navMiddle = getElement("nav-middle");
+    let navLinkTexts = document.getElementsByClassName("nav-link-text");
+
+    togglerBtn.addEventListener('click', () => {
+        nav.classList.toggle("nav-bar-small");
+
+        if (toggle) {
+            logoLong.classList.add("hidden");
+            navMiddle.classList.add("hidden");
+            togglerBtn.classList.add("toggler-rotate");
+            for (i = 0; i < navLinkTexts.length; i++) {
+                navLinkTexts[i].classList.add("hidden");
+            }
+            toggle = false;
+        } else {
+            logoLong.classList.remove("hidden");
+            navMiddle.classList.remove("hidden");
+            togglerBtn.classList.remove("toggler-rotate");
+            for (i = 0; i < navLinkTexts.length; i++) {
+                navLinkTexts[i].classList.remove("hidden");
+            }
+            toggle = true;
+        }
+    })
+</script>
 
 </html>
