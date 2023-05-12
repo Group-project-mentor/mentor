face659a814d04d8e69ad4bf1575e1a172d9a519
kavishka-sulakshana <kss20001218@gmail.com>
2023-05-12 13:16:16 +0530

    do one important change in topics

--- app/controllers/rcResources.php
+++ app/controllers/rcResources.php
@@ -228,12 +228,12 @@ class RcResources extends Controller
 
     public function organized($grade_id, $subject_id)
     {
-        $topics = $this->model("resourceModel")->getTopics($grade_id, $subject_id);
-        $topicOrderRow = $this->model("resourceModel")->getTopicOrder($grade_id, $subject_id);
-        $topicOrder = (!empty($topicOrderRow))?$topicOrderRow->tpcOrder:null;
         $this->getNames($grade_id, $subject_id);
         $_SESSION["gid"] = $grade_id;
         $_SESSION["sid"] = $subject_id;
+        $topics = $this->model("resourceModel")->getTopics($grade_id, $subject_id);
+        $topicOrderRow = $this->model("resourceModel")->getTopicOrder($grade_id, $subject_id);
+        $topicOrder = (!empty($topicOrderRow))?$topicOrderRow->tpcOrder:null;
         if(empty($topicOrderRow)){
             if(!empty($topics)){
                 foreach ($topics as $topic) {
@@ -254,11 +254,13 @@ class RcResources extends Controller
         foreach ($topics as $topic) {
             $topicData[$topic->id] = $topic;
         }
+        print_r($topicData);
+        print_r($topicOrder);
         $this->view('resourceCtr/resources/organized',array($topicData,$topicOrder));
     }
 
     public function getResourcesTopics(){
-        $resourcesTopicWise = $this->model("resourceModel")->getResourcesWithTopics($_SESSION['gid'], $_SESSION['sid']);
+        $resourcesTopicWise = $this->model("resourceModel")->getResourcesWithTopics($_SESSION['sid'], $_SESSION['gid']);
         $organizedList = array();
         if(!empty($resourcesTopicWise)){
             foreach ($resourcesTopicWise as $topic) {
